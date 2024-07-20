9d79732666e2f2961d818e3cf4f3acb277f38cef
risv1 <rishi.aangarai@gmail.com>
2024-07-20 18:35:37 +0530

    fix: naming

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,17 +5,17 @@ import Link from "next/link"
 export default function Navbar() {
     const links = [
         {id: 1, path: "#about", name: "About Us"},
-        {id: 2, path: "#domains", name: "Events"},
+        {id: 2, path: "#domains", name: "Domains"},
         {id: 3, path: "#events", name: "Events"},
         {id: 4, path: "#team", name: "Team"},
-        {id: 5, path: "#memberships", name: "Memberships"},
+        {id: 5, path: "#memberships", name: "Membership"},
         {id: 6, path: "#contact", name: "Contact Us"}
     ]
 
     return (
         <div className="w-full h-20 flex bg-inherit items-center">
             <Image src={logo} alt="Logo" className="lg:ml-10 ml-5 " width={60} height={60} />
-            <div className="lg:w-[60%] gap-3 md:w-[80%] sm:w-full ml-auto mr-auto flex flex-row justify-evenly">
+            <div className="lg:w-[50%] gap-3 md:w-[80%] sm:w-full ml-auto mr-auto flex flex-row justify-evenly">
                 {links.map(link => (
                     <Link href={link.path} key={link.id} className="font-semibold lg:text-xl md:text-lg text-xs">
                         {link.name}
