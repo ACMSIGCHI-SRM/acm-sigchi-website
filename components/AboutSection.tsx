ac9e603c33bbdb1124b97eba61bd3755f741a8d8
risv1 <rishi.aangarai@gmail.com>
2024-07-21 15:37:09 +0530

    feat: add about section

---
+++ components/AboutSection.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function AboutSection() {
+  return (
+    <div className="w-full h-fit flex flex-col items-center">
+      <h1 className="font-bold text-4xl text-[#EAA137]">About Us</h1>
+      <div className="mt-10 w-[80%] flex flex-row justify-between items-center">
+        <div className="w-1/2 flex flex-col gap-4">
+          <p className="w-[85%] font-semibold xl:text-2xl lg:text-xl md:text-lg sm:text-md text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+            molestiae sequi praesentium recusandae eius?
+          </p>
+          <p className="w-[85%] font-normal xl:text-xl lg:text-lg md:text-md sm:text-sm text-xs">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repellendus similique beatae tempora, animi id nesciunt, minus
+          </p>
+          <button className="w-fit px-2 py-2 bg-[#5080C6] rounded-full">Learn more</button>
+        </div>
+        <div className="w-1/2 ml-auto flex items-center justify-center">
+            <Image src="/logo.svg" alt="About Us" width={700} height={700} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import AboutSection from "@/components/AboutSection";
+import IntroSection from "@/components/IntroSection";
 import MembershipCard from "@/components/MembershipCard";
 import Navbar from "@/components/Navbar";
 import TeamSection from "@/components/TeamSection";
@@ -8,14 +10,21 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`w-screen h-screen ${inter.className}`}
+      className={`w-screen h-screen overflow-x-hidden ${inter.className}`}
     >
       <Navbar />
-      <h1 className="text-4xl font-bold">ACM Sigchi</h1>
-      <div className="w-full h-fit flex justify-center gap-6">
+      <div className="mt-10 p-5 w-full h-fit flex justify-center">
+        <IntroSection />
+      </div>
+      <div id="about" className="mt-28 p-5 w-full h-fit flex justify-center">
+        <AboutSection />
+      </div>
+      <div id="team" className="mt-28 p-5 w-full h-fit flex justify-center gap-6">
         <TeamSection />
       </div>
-      <MembershipCard />
+      <div id="memberships" className="mt-28 p-5 w-full h-fit flex justify-center">
+        <MembershipCard />
+      </div>
     </main>
   );
 }
