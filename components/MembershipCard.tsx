7973769031f01358dd0a78b3a697a3afa3b9d983
vinay-04 <vinayrajan04@gmail.com>
2024-07-21 14:17:35 +0530

    fix: add text to

--- components/MembershipCard.tsx
+++ components/MembershipCard.tsx
@@ -4,9 +4,9 @@ export default function MembershipCard() {
     return (
         <div className="w-full justify-center flex">
         <div className="w-[50%] h-fit flex-col bg-[#FFF4D9] rounded-lg">
-            <h1 className="text-5xl font-bold text-black p-8">ACM Paid Membership</h1>
-            <div className="p-8 text-xl text-black">
-                    Commodo excepteur cupidatat culpa aute officia consectetur aliqua mollit elit eiusmod magna qui laborum proident. Magna aute velit id nostrud ad aute aliqua mollit duis adipisicing veniam sint velit velit. Adipisicing magna labore ea commodo et Lorem elit irure fugiat minim deserunt tempor quis. Ad deserunt laboris aute eiusmod deserunt id ullamco dolore magna. Dolor non adipisicing amet sint non incididunt. Velit ad consectetur officia esse occaecat exercitation qui est sint proident cillum exercitation.
+            <h1 className="text-5xl font-bold text-black p-8 pb-2">ACM Paid Membership</h1>
+            <div className="p-8 pb-2 text-xl text-black">
+            ACM offers the resources, access and tools to invent the future. No one has a larger global network of professional peers. No one has more exclusive content. No one presents more forward-looking events. Or confers more prestigious awards. Or provides a more comprehensive learning center. 
             </div>
             <Link href={'https://www.google.com'}>
             <div className="justify-start flex p-8">
