65f9ff39d2ac234731461a16c438ad8b0f08dcd3
risv1 <rishi.aangarai@gmail.com>
2024-07-22 10:41:56 +0530

    feat: add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,27 +4,44 @@ import MembershipCard from "@/components/MembershipCard";
 import Navbar from "@/components/Navbar";
 import TeamSection from "@/components/TeamSection";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`w-screen h-screen overflow-x-hidden ${inter.className}`}
-    >
-      <Navbar />
-      <div className="mt-10 p-5 w-full h-fit flex justify-center">
-        <IntroSection />
-      </div>
-      <div id="about" className="mt-28 p-5 w-full h-fit flex justify-center">
-        <AboutSection />
-      </div>
-      <div id="team" className="mt-28 p-5 w-full h-fit flex justify-center gap-6">
-        <TeamSection />
-      </div>
-      <div id="memberships" className="mt-28 p-5 w-full h-fit flex justify-center">
-        <MembershipCard />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>ACM SIGCHI</title>
+        <meta
+          name="description"
+          content="A community of developers, designers, and creators."
+        />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <main
+        className={`w-screen h-screen overflow-x-hidden ${inter.className}`}
+      >
+        <Navbar />
+        <div className="mt-10 p-5 w-full h-fit flex justify-center">
+          <IntroSection />
+        </div>
+        <div id="about" className="mt-28 p-5 w-full h-fit flex justify-center">
+          <AboutSection />
+        </div>
+        <div
+          id="team"
+          className="mt-28 p-5 w-full h-fit flex justify-center gap-6"
+        >
+          <TeamSection />
+        </div>
+        <div
+          id="memberships"
+          className="mt-28 p-5 w-full h-fit flex justify-center"
+        >
+          <MembershipCard />
+        </div>
+      </main>
+    </>
   );
 }
